8acc7f2910def1b53c4cfc72e433abdcaf87b625
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-12-20 18:02:21 +0100

    add banner-slider in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,6 +7,92 @@ export default{
 
 <template>
     <footer>
+        <div class="slider">
+            <div class="slide-track">
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="../../public/img/logo-banner.png" height="100" width="250" alt="" />
+                </div>
+
+                <!-- <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+                </div> -->
+            </div>
+        </div>
         <div class="container">
             <div class="row pt-4">
                 <div class="col d-flex justify-content-center">
@@ -70,6 +156,68 @@ export default{
 
 footer{
     background-color: yellow;
+
+
+
+    @mixin white-gradient {
+        background: linear-gradient(to right,  rgba(255,255,255,1) 0%,rgba(255,255,255,0) 100%);
+    }
+
+    $animationSpeed: 40s;
+
+    // Animation
+    @keyframes scroll {
+        0% { transform: translateX(0); }
+        100% { transform: translateX(calc(-250px * 7))}
+    }
+
+
+    // Styling
+    .slider {
+        background: white;
+        box-shadow: 0 10px 20px -5px rgba(0, 0, 0, .125);
+        height: 100px;
+        margin: auto;
+        overflow:hidden;
+        position: relative;
+        width: 100%;
+        
+        &::before,
+        &::after {
+            @include white-gradient;
+            content: "";
+            height: 100px;
+            position: absolute;
+            width: 200px;
+            z-index: 2;
+        }
+        
+        &::after {
+            right: 0;
+            top: 0;
+            transform: rotateZ(180deg);
+        }
+
+        &::before {
+            left: 0;
+            top: 0;
+        }
+        
+        .slide-track {
+            animation: scroll $animationSpeed linear infinite;
+            display: flex;
+            width: calc(250px * 14);
+        }
+        
+        .slide {
+            height: 100px;
+            width: 250px;
+        }
+    }
+
+
+
+
    
     .row{
         .col{
--- src/pages/Video.vue
+++ src/pages/Video.vue
@@ -4,38 +4,13 @@ export default{
 
     data() {
         return {
-            menuItems: [
-                {
-                    image: 'DSCF9269.JPG',
-                    category: 'People',
-                    routeName: 'people',
-                },
-                {
-                    image: 'DSCF8452.JPG',
-                    category: 'Hospitality Industry',
-                    routeName: 'hospitality',
-                },
-                {
-                    image: 'DSCF1155.jpg',
-                    category: 'Wedding',
-                    routeName: 'wedding',
-                },
-                {
-                    image: 'DSCF9076.JPG',
-                    category: 'Commercial',
-                    routeName: 'commercial',
-                },
-                {
-                    image: 'FUJIFILM_X-T30_4160x6240_053824.jpg',
-                    category: 'Events',
-                    routeName: 'events',
-                },
-                {
-                    image: 'FUJIFILM_X-T30_4160x6240_052958.jpg',
-                    category: 'Street Photography',
-                    routeName: 'street',
-                },
-            ]
+            // menuItems: [
+            //     {
+            //         label: 'candles3.webp',
+                    
+            //     },
+    
+            // ]
         }
     },
     methods: {
@@ -54,25 +29,13 @@ export default{
             <div class="row">
                 <h1>prova</h1>
 
-                <!-- CARD V-FOR -->
-
-                <!-- <div v-for="(item, index) in menuItems" :key="index" class="col-lg-4 col-sm-12 p-1" data-aos="fade-up"> -->
-
-                    <!-- click su tutta l'immagine -->
-
-                    <!-- <router-link :to="{name: item.routeName}">
+                <!-- <div v-for="(i, index) in candles" :key="index" class="col width-4">
+                    <div class="candles">
+                        <img :src="getImagePath(i.label)" alt="candles">
                         
-                    </router-link> -->
-                    
-                    <!-- <div class="hero-image" :style="{ backgroundImage: 'url(' + getImagePath(item.image) + ')' }">
-                        <div class="hero-text">
-                            <h3 class="pb-3">{{item.category}}</h3>
-                            <router-link :to="{name: item.routeName}">
-                                <button>Look</button>
-                            </router-link>
-                        </div>
                     </div>
                 </div> -->
+
             </div>
         </div>
     </section>
@@ -81,49 +44,6 @@ export default{
 <style lang="scss" scoped>
 @use '../styles/app.scss' as *;
 
-.row{
 
-    .hero-image{
-        background-color: #cccccc;
-        height: 700px;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        position: relative;
-        transform: scale(1);
-        transition: all 0.5s;
-
-        &:hover{
-            transform: scale(1.02);
-        }
-    }
-
-    .hero-text{
-        text-align: center;
-        position: absolute;
-        top: 10%;
-        left: 50%;
-        transform: translate(-50%, -10%);
-        color: white;
-
-        h3{
-            font-size: 30px;
-            font-weight: 400;
-            white-space: nowrap;
-        }
-
-        button{
-            border: 1px solid white;
-            background-color: transparent;
-            padding: 8px 70px;
-            color: white;
-            transition: 0.3s;
-
-            &:hover{
-                box-shadow: 0 0 6px 3px rgb(239, 239, 211);
-            }
-        }
-    }    
-}
 
 </style>
